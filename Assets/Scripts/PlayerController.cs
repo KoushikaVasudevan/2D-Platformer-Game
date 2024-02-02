f0bbeea35abcf46249db60919f551dbd3bf19395
DESKTOP-6U5DPEC\koush <koushikavasudevan@gmail.com>
2024-02-02 16:53:04 +0530

    player moves horizontally.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
 
+    public float speed;
+
     private void Awake()
     {
         Debug.Log("Player Controller Awake");
@@ -14,24 +16,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        float jump = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-        animator.SetFloat("Jump", Mathf.Abs(jump));
+        PlayerMovement(horizontal);
+        PlayerAnimation(horizontal, vertical);
+    }
 
+    private void PlayerMovement(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayerAnimation(float horizontal, float vertical)
+    {
+        //Player idle/run
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
-
         transform.localScale = scale;
 
+        //Player jump
+        if(vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        //Player crouch
         if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl))
         {
             animator.SetTrigger("Crouch");
